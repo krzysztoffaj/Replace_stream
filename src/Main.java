e0e8776e4d98c26e280ef01975b87c94d7b16377
Fajeczny <krzysztof.faj@gmail.com>
2017-03-02 18:56:36 +0100

    Selecting directory correctly returns all files with certain extension

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Aplikacja");
-        primaryStage.setScene(new Scene(root, 650, 700));
+        primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
     }
 
